cb845c3c85971744914c07fe819e24e9dae940d8
Andip29 <priyandi.zembarazizi@gmail.com>
2021-02-12 10:45:42 +0700

    Menyelesaikan video pertemuan 2

---
+++ Pertemuan2/index.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Priyandi Zembar Azizi
+203040070
+https://github.com/Andip29/pw2021_203040070
+Pertemuan 2 - 12 Februari 2021
+mempelajari sintaks php
+*/
+?>
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar output
+// echo, print
+// print_
+// var_dump
+
+// echo "Priyandi Zembar Azizi";
+// print "Priyandi Zembar Azizi";
+// print_r("Priyandi Zembar Azizi");
+// var_dump ("Priyandi Zembar Azizi");
+
+// echo 123;
+// echo true;
+// echo false;
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PPHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+// $nama = "Priyandi Zembar Azizi";
+// echo 'Halo, nama saya $nama';
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan ="Priyandi";
+// $nama_belakang = "Zembar Azizi";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Priyandi";
+// $nama .= " ";
+// $nama .= "Zembar Azizi"
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 < 5);
+// var_dump(1 > 5);
+// var_dump(1 == 5);
+// var_dump(1 == "1");
+
+// identitas
+// ===,!==
+// var_dump(1 === "1");
+
+// Logika
+// &&,||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo, Selamat Datang <?php echo $nama; ?></h1>
+</body>
+</html> -->
