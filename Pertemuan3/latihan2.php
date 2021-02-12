10c94010bc48eb3c3b37c9bef84197d9afd08f6e
Andip29 <priyandi.zembarazizi@gmail.com>
2021-02-12 22:11:15 +0700

    Menyelesaikan pertemuan3 struktur kendali php dan menambahkan nama

--- Pertemuan3/latihan2.php
+++ Pertemuan3/latihan2.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Priyandi Zembar Azizi
+203040070
+https://github.com/Andip29/pw2021_203040070
+Pertemuan 2 - 12 Februari 2021
+mempelajari sintaks php
+*/
+?>
+<?php
 // Pengkondisian / Percabangan
 // if else
 // if else if else
