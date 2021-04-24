3b39bf722b05e00a5f81d2081c12e5e5e394f9ed
Andip29 <priyandi.zembarazizi@gmail.com>
2021-04-24 21:45:30 +0700

    upload pertemuan 10

---
+++ Pertemuan10/latihan2.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Priyandi Zembar Azizi
+203040070
+https://github.com/Andip29/pw2021_203040070
+Pertemuan 10 - (22 APRIL 2021)
+Materi pertemuan 10 mempelajari mengenai koneksi database dan insert data 
+*/
+?>
+ <?php
+  require 'functions.php';
+  $mahasiswa = query("SELECT * FROM mahasiswa");
+  ?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Daftar Mahasiswa</title>
+ </head>
+
+ <body>
+   <h3>Daftar Mahasiswa</h3>
+
+   <table border="1" cellpadding="10" cellspacing="0">
+     <tr>
+       <th>#</th>
+       <th>Gambar</th>
+       <th>NRP</th>
+       <th>Nama</th>
+       <th>Email</th>
+       <th>Jurusan</th>
+       <th>Aksi</th>
+     </tr>
+
+     <?php $i = 1;
+      foreach ($mahasiswa as $m) : ?>
+       <tr>
+         <td><?= $i++; ?></td>
+         <td><img src="img/<?= $m['gambar']; ?>" width="90"></td>
+         <td><?= $m['nrp']; ?></td>
+         <td><?= $m['nama']; ?></td>
+         <td><?= $m['email']; ?></td>
+         <td><?= $m['jurusan']; ?></td>
+         <td>
+           <a href="">ubah</a> | <a href="">hapus</a>
+         </td>
+       </tr>
+     <?php endforeach; ?>
+   </table>
+
+ </body>
+
+ </html>
